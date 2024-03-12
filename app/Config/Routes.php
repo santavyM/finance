bcee078f45be0794f2d428d0ee465b2ea1e4601b
mettle353 <martin.santavy@email.cz>
2024-03-12 10:52:50 +0100

    update

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -40,23 +40,58 @@ $routes->get('post/(:any)','BlogController::readPost/$1',['as'=>'read-post']);
 $routes->get('category/(:any)','BlogController::categoryPosts/$1',['as'=>'category-posts']);
 $routes->get('tag/(:any)','BlogController::tagPosts/$1',['as'=>'tag-posts']);
 $routes->get('search','BlogController::searchPosts',['as'=>'search-posts']);
+$routes->get('contact-us','BlogController::contactUs',['as'=>'contact-us']);
+$routes->post('contact-us','BlogController::contactUsSend',['as'=>'contact-us-send']);
 
 $routes->group('admin', static function($routes){
-    $routes->group('', ['filter'=>'cifilter:auth'], static function($routes){
-        //$routes->get('example-page','Home::method3');
-        $routes->get('home', 'AdminController::index', ['as' => 'admin.home']);
-        $routes->get('logout', 'AdminController::LogoutHandler', ['as' => 'admin.logout']);
-    });
 
-    $routes->group('', ['filter'=>'cifilter:guest'], static function($routes) {
-        //$routes->get('example-auth','Home::method4');
-        $routes->get('login', 'AuthController::loginForm', ['as'=>'admin.login.form']);
-        $routes->post('login', 'AuthController::loginHandler', ['as'=>'admin.login.handler']);
-        $routes->get('forgot-password', 'AuthController::forgotForm', ['as'=>'admin.forgot.form']);
-        $routes->post('forgot-password', 'AuthController::sendPasswordResetLink', ['as'=>'send_password_reset_link']);
-        $routes->get('password/reset/(:any)', 'AuthController::resetPassword/$1', ['as'=>'admin.reset-password']);
-        $routes->post('password/reset/(:any)','AuthController::resetPasswordHandler/$1');
-    });
+       $routes->group('', ['filter'=>'cifilter:auth'], static function($routes){
+        //    $routes->view('example-page','example-page');
+          $routes->get('home','AdminController::index',['as'=>'admin.home']);
+          $routes->get('logout','AdminController::logoutHandler',['as'=>'admin.logout']);
+          $routes->get('profile','AdminController::profile',['as'=>'admin.profile']);
+          $routes->post('update-personal-details','AdminController::updatePersonalDetails',['as'=>'update-personal-details']);
+          $routes->post('update-profile-picture','AdminController::updateProfilePicture',['as'=>'update-profile-picture']);
+          $routes->get('settings','AdminController::settings',['as'=>'settings']);
+          $routes->post('update-general-settings','AdminController::updateGeneralSettings',['as'=>'update-general-settings']);
+          $routes->post('update-blog-logo','AdminController::updateBlogLogo',['as'=>'update-blog-logo']);
+          $routes->post('update-blog-favicon','AdminController::updateBlogFavicon',['as'=>'update-blog-favicon']);
+          $routes->post('update-social-media','AdminController::updateSocialMedia',['as'=>'update-social-media']);
+          $routes->post('update-calculators','AdminController::updateCalculators',['as'=>'update-calculators']);
+          $routes->post('upload-theme', 'AdminController::uploadTheme', ['as' => 'upload-theme']);
+          $routes->post('select-theme', 'AdminController::selectTheme', ['as' => 'select-theme']);
+          //kategorie
+          $routes->get('categories','AdminController::categories',['as'=>'categories']);
+          $routes->post('add-category','AdminController::addCategory',['as'=>'add-category']);
+          $routes->get('get-categories','AdminController::getCategories',['as'=>'get-categories']);
+          $routes->get('get-category','AdminController::getCategory',['as'=>'get-category']);
+          $routes->post('update-category','AdminController::updateCategory',['as'=>'update-category']);
+          $routes->get('delete-category','AdminController::deleteCategory',['as'=>'delete-category']);
+          $routes->get('reorder-categories','AdminController::reorderCategories',['as'=>'reorder-categories']);
+          $routes->get('get-parent-categories','AdminController::getParentCategories',['as'=>'get-parent-categories']);
+
+          //Posty
+          $routes->group('posts', static function($routes){
+              $routes->get('new-post','AdminController::addPost',['as'=>'new-post']);
+              $routes->post('create-post','AdminController::createPost',['as'=>'create-post']);
+              $routes->get('/','AdminController::allPosts',['as'=>'all-posts']);
+              $routes->get('get-posts','AdminController::getPosts',['as'=>'get-posts']);
+              $routes->get('edit-post/(:any)','AdminController::editPost/$1',['as'=>'edit-post']);
+              $routes->post('update-post','AdminController::updatePost',['as'=>'update-post']);
+              $routes->get('delete-post','AdminController::deletePost',['as'=>'delete-post']);
+          });
+          
+       });
+
+       $routes->group('', ['filter'=>'cifilter:guest'], static function($routes){
+            // $routes->view('example-auth','example-auth');
+          $routes->get('login','AuthController::loginForm',['as'=>'admin.login.form']);
+          $routes->post('login','AuthController::loginHandler',['as'=>'admin.login.handler']);
+          $routes->get('forgot-password','AuthController::forgotForm',['as'=>'admin.forgot.form']);
+          $routes->post('forgot-password','AuthController::sendPasswordResetLink',['as'=>'send_password_reset_link']);
+          $routes->get('password/reset/(:any)','AuthController::resetPassword/$1',['as'=>'admin.reset-password']);
+          $routes->post('reset-password-handler/(:any)','AuthController::resetPasswordHandler/$1',['as'=>'reset-password-handler']);
+        });
 });
 
 /*
